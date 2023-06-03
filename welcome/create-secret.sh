26a5358a4aa2c480afa962bd136eda304b17e4af
rob <robertasones@hotmail.com>
2023-06-03 10:23:20 +0100

    Welcome devfile 14

--- welcome/create-secret.sh
+++ welcome/create-secret.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 if [ -f welcome/git-credentials ]; then
-  if grep XXXXXXXX welcome/git-credentials; then
-    echo " welcome/git-credentials file has not been updated with the PAT or saved since the update"
+  if grep -q XXXXXXXX welcome/git-credentials; then
+    echo " The git-credentials Template file in the welcome folder has not been updated with the PAT or saved since being updated"
     exit 1
   fi
-  echo " doing stuff"
+  oc get secret git-credentials-secret
+  if [ $? -eq 0 ]; then
+    echo "Secret exists, will replace with new values"
+  else
+     echo "Creating secret"
+  fi
 else
-  echo "Template file has not been created please run 01-create-template task before this one"
+  echo "Template file has not been created please run task 01-create-template task before this one"
   exit 1
 fi
   
