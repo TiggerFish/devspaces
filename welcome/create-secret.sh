f651706b15854fd155f0bac9feb72a3bf0b2f1c5
rob <robertasones@hotmail.com>
2023-06-03 09:45:01 +0100

    Welcome devfile 11

--- welcome/create-secret.sh
+++ welcome/create-secret.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-touch me
+if [ -f welcome/git-credentials ]; then
+  if grep XXXXXX welcome/git-credentials; then
+    echo " welcome/git-credentials file has not been updated with the PAT or saved since the update"
+    exit 1
+  fi
+  echo " doing stuff"
+else
+  echo "Template file has not been created please run 01-create-template task before this one"
+  exit 1
+fi
+  
