4be7d8bd24704003779d5f7e1a9018dcaab152c8
rob <robertasones@hotmail.com>
2023-06-03 11:52:58 +0100

    Welcome devfile 22

--- welcome/create-template.sh
+++ welcome/create-template.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "https://$(oc whoami):XXXXXXXX@gitlab.com" > welcome/git-credentials
 echo "Please edit the welcome/git-credentials template file, replacing XXXXXXXX with your git PAT"
-echo "Once you have done this please run the devfile/02-Create-Secret task"
+echo "Once you have done this please run the devfile/02-create-secret task"
 echo
